bd3bb65ad9f63b41885b4e17cf0a21ca74cd31c8
hafiz-anas0 <hafizanasmajid2006@gmail.com>
2026-08-11 16:54:29 +0500

    Initialize MERU project

---
+++ backend/src/app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+    res.json({
+        message: "MERU API is running",
+    });
+});
+
+module.exports = app;
